6297143c1f2578b4d1bf473ff78e3390d6f7ed3e
Konrad Sudyn <ksudyn@c3r7s1.42madrid.com>
2025-12-04 19:42:23 +0100

    game_of_life cambiado

--- Rank_05/nivel_2/game_of_life/Otro_por_ver.c
+++ Rank_05/nivel_2/game_of_life/Otro_por_ver.c
@@ -6,7 +6,7 @@
 /*   By: ksudyn <ksudyn@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 18:42:11 by ksudyn            #+#    #+#             */
-/*   Updated: 2025/12/03 18:45:41 by ksudyn           ###   ########.fr       */
+/*   Updated: 2025/12/04 18:34:09 by ksudyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-static void printboard(char *board, int w, int h)
+static void printboard(char *board, int width, int height)
 {
-	for (int y = 0; y < h; y++)
+	for (int y = 0; y < height; y++)
 	{
-		for (int x = 0; x < w; x++)
+		for (int x = 0; x < width; x++)
 		{
-			if (board[y * w + x])
-				putchar('O');
+			if (board[y * width + x])
+				putchar('0');
 			else
 				putchar(' ');
 		}
@@ -29,44 +29,49 @@ static void printboard(char *board, int w, int h)
 	}
 }
 
-static int count_neighbor(char *board, int w, int h, int x, int y)
+/* Cuenta los vecinos vivos de la celda (col, row).
+   Las posiciones fuera del tablero son muertas */
+static int count_neighbor(char *board, int width, int height, int x, int y)
 {
-	int cnt = 0;
+	int count = 0;
 	for (int dy = -1; dy <= 1; dy++)
 	{
 		for (int dx = -1; dx <= 1; dx++)
 		{
 			if (dy == 0 && dx == 0)
 				continue;
-			int nx = x + dx;
-			int ny = y + dy;
-			if(nx >= 0 && nx < w && ny >= 0 && ny < h)
-				cnt += board[ny * w + nx];
+			int neighbor_x = x + dx;
+			int neighbor_y = y + dy;
+			if(neighbor_x >= 0 && neighbor_x < width &&
+				neighbor_y >= 0 && neighbor_y < height)
+			{
+				count += board[neighbor_y * width + neighbor_x];
+			}
 		}
 	}
-	return cnt;
+	return count;
 }
 
-static void step(char *board, char * next, int w, int h)
+static void step(char *board, char * next_board, int width, int height)
 {
-	for (int y = 0; y < h; y++)
+	for (int y = 0; y < height; y++)
 	{
-		for (int x = 0; x < w; x++)
+		for (int x = 0; x < width; x++)
 		{
-			int n = count_neighbor(board, w, h, x, y);
-			if (board[y * w + x])
+			int n = count_neighbor(board, width, height, x, y);
+			if (board[y * width + x])//fila * ancho + columna
 			{
 				if (n == 2 || n == 3)
-					next[y * w + x] = 1;
+					next_board[y * width + x] = 1;
 				else
-					next[y * w + x] = 0;
+					next_board[y * width + x] = 0;
 			}
 			else
 			{
 				if (n == 3)
-					next[y * w + x] = 1;
+					next_board[y * width + x] = 1;
 				else
-					next[y * w + x] = 0;
+					next_board[y * width + x] = 0;
 			}
 		}
 	}
@@ -77,45 +82,45 @@ int main(int ac, char **av)
 	if(ac != 4)
 		return 1;
 
-	int w = atoi(av[1]);
-	int h = atoi(av[2]);
+	int width = atoi(av[1]);
+	int height = atoi(av[2]);
 	int iter = atoi(av[3]);
 
-	char *board = calloc(w*h, 1);
-	char *next = calloc(w*h, 1);
+	char *board = calloc(width * height, 1);
+	char *next_board = calloc(width * height, 1);
 
-	if(!board || !next)
+	if(!board || !next_board)
 		return 1;
 
-	int x = 0, y = 0, pen = 0;
+	int x = 0, y = 0, pencil = 0;
 	char c;
 
 	while (read(0, &c, 1) > 0)
 	{
 		if(c == 'x')
-			pen = !pen;
+			pencil = !pencil;
 		if(c == 'a' && x > 0)
 			x--;
-		else if(c == 'd' && x < w -1)
+		else if(c == 'd' && x < width -1)
 			x++;
 		else if(c == 'w' && y > 0)
 			y--;
-		else if(c == 's' && y < h -1)
+		else if(c == 's' && y < height -1)
 			y++;
-		if(pen)
-			board[y * w + x] = 1;
+		if(pencil)
+			board[y * width + x] = 1;
 	}
 
 	for(int i = 0; i < iter; i++)
 	{
-		step(board, next, w, h);
+		step(board, next_board, width, height);
 		char *temp = board;
-		board = next;
-		next = temp;
+		board = next_board;
+		next_board = temp;
 	}
 	
-	printboard(board, w, h);
+	printboard(board, width, height);
 	free(board);
-	free(next);
+	free(next_board);
 	return 0;
 }
